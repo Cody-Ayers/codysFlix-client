c9671b0395132894050c4b16536db74869a27092
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2023-12-07 21:38:53 -0700

    changed to fetch from api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,57 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "The Dark Knight",
-            image:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKjUEHR-_M2mRZRAy9riapfsEHZevoE3sh-O-qeIyi&s",
-            description: "A superhero epic in the Batman trilogy",
-            director: "Christopher Nolan",
-            genre: "Action"
-        },
-        {
-            id: 2,
-            title: "Interstellar",
-            image:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDgmUCNxQw7adsAAGIFulSGE4ZUEN3ZF1y3cDKC-WU&s",
-            description: "A space exploration and time-bending adventure",
-            director: "Christopher Nolan",
-            genre: "Sci-Fi"
-        },
-        {
-            id: 3,
-            title: "The Godfather",
-            image:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ94fHo9ZHfdGTLQXxJNDf3uxMjR_3mcPeis16caRR8&s",
-            description: "A story about mob boss Don Corleone turning over his empire to his son.",
-            director: "Francis Ford Coppola",
-            genre: "Crime"
-        },
-        {
-            id: 4,
-            title: "The Social Network",
-            image:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST9Jh6B745NDHgvssbHMQEPrsrbNByC8juo9TbFZbl&s",
-            description: "A biographical drama about the founding of Facebook",
-            director: "David Fincher",
-            genre: "Drama"
-        },
-        {
-            id: 5,
-            title: "The Matrix",
-            image:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_e09w3ItoTsp1vpWolzKADk9U_6zE0TBNVSU3hYYL&s",
-            description: "A groundbreaking sci-fi action film about a red or blue pill",
-            director: "Lana Wachowski",
-            genre: "Action"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect (() => {
+        fetch("https://codys-flix-0b23a40a1d0d.herokuapp.com/movies")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.docs.map((doc) => {
+                return {
+                    _id: movie.id,
+                    Title: movie.Title,
+                    Description: movie.Description,
+                    Genre: {
+                        Name: movie.Genre.Name,
+                        Description: movie.Genre.Description
+                    },
+                    Director: {
+                        Name: movie.Director.Name,
+                        Bio: movie.Director.Bio,
+                        BirthDate: movie.Director.BirthDate,
+                        DeathDate: movie.Director.DeathDate
+                    },
+                    Featured: movie.Featured,
+                    ImageURL: movie.ImageURL
+                };
+            });
+
+            setMovies(moviesFromApi);
+        });
+    }, []);
 
     if (selectedMovie) {
         return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -9,3 +11,22 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         </div>
     );
 };
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      director: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        bio: PropTypes.string.isRequired,
+        birthdate: PropTypes.number.isRequired,
+        deathdate: PropTypes.number,
+      }),
+      genre: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+      }),
+    }).isRequired,
+    onBookClick: PropTypes.func.isRequired,
+  };
